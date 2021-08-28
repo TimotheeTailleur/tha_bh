7ce226eb050079060de5487eb6cc653fbb1f1406
Timothée Tailleur <tim.tailleur@gmail.com>
2021-08-28 17:13:45 +0200

    Init project structure

---
+++ src/main/java/com/bh/tha/ThaBhApplication.java
@@ -0,0 +1,13 @@
+package com.bh.tha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThaBhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThaBhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bh/tha_bh/ThaBhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bh.tha_bh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThaBhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
